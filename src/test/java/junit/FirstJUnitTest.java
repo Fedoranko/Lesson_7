86329619b0bccf9cd44dc33e506b3d623e80fc26
frdor <galushin@sel-be.ru>
2024-02-17 12:32:27 +0300

    After and Before

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,32 @@
+package junit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnitTest {
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("beforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("afterEach");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("Test");
+        Assertions.assertTrue(3 > 2);
+    }
+}
+
+
+
+
+
+
+
+
